e432fa9e6c9f4f74adbbaafc37de48dea93a9a31
Thi quynh Vo <yenfrance1703@gmail.com>
2018-04-11 20:26:26 +0000

    automatically backed up by learn

--- home.js
+++ home.js
@@ -7,3 +7,19 @@ class Comment extends React.Component {
     )
   }
 }
+class BlogPost extends React.Component {
+render(){
+  return(
+    <div>
+    <div id="blog-content">
+          Dear Reader: Bjarne Stroustrup has the perfect lecture oration.
+        </div>
+
+        <Comment content={"I agree with this statement. - Angela Merkel"}/>
+        <Comment content={"A universal truth. - Noam Chomsky"}/>
+        <Comment content={"Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451"}/>
+    </div>
+  )
+}
+
+}
